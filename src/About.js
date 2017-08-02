7e995c62a388580f3d351a7ca0e55d04ee0ec33b
Ken Torry <kentorry@afrodesiamedia.com>
2017-08-02 16:12:00 -0500

    first commit

---
+++ src/About.js
@@ -0,0 +1,22 @@
+import React, { Component } from 'react';
+
+
+class About extends Component {
+  render() {
+    return (
+      <div className="Home">
+         <h1> About </h1>
+
+         <ul> 
+          <li>test 1</li>
+          <li>test 1</li>
+          <li>test 1</li>
+        </ul>
+
+        
+      </div>
+    );
+  }
+}
+
+export default About;
---
+++ src/Home.js
@@ -0,0 +1,23 @@
+import React, { Component } from 'react';
+// import logo from './react.svg';
+// import './Home.css';
+
+class Home extends Component {
+  render() {
+    return (
+      <div className="Home">
+         <h1> Test Me </h1>
+
+         <ul> 
+          <li>test 1</li>
+          <li>test 1</li>
+          <li>test 1</li>
+        </ul>
+
+        
+      </div>
+    );
+  }
+}
+
+export default Home;
